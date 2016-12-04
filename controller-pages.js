8384e53834dcccd853a4ef4cb0a5fa3e0718c51d
gingerP <new.vinni@gmail.com>
2016-12-04 14:51:04 +0300

    - mobile size

--- controller-pages.js
+++ controller-pages.js
@@ -4,14 +4,14 @@ var handlers = [
         path: ['/'],
         fn: function (req, res) {
             console.log('Request: ' + req.path);
-            res.sendFile(__dirname + '/client/views/index_dev.html');
+            res.sendFile(__dirname + '/client/views/index.html');
         }
     }
 ];
 
 module.exports = {
     init: function (app) {
-        app.use('/static', express.static(__dirname + '/client'));
+        app.use('/static', express.static(__dirname + '/build'));
         app.use('/node_modules', express.static(__dirname + '/node_modules'));
         handlers.forEach(function (handler) {
             app.get(handler.path,
